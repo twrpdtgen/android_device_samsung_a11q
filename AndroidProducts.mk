2f04c8c307573a2547cec4eccd42e1f679cb0b10
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-30 09:35:11 +0100

    a11q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-A115M Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a11q.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a11q-user \
+    omni_a11q-userdebug \
+    omni_a11q-eng
